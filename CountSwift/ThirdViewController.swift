57a9de598a6a56cabf5c0f14862600fefa93b558
KOTARO SUTO <ng1a5ss43@gmail.com>
2015-02-27 18:35:41 +0900

    Add UI

---
+++ CountSwift/ThirdViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ThirdViewController.swift
+//  CountSwift
+//
+//  Created by Kotaro Suto on 2015/02/27.
+//  Copyright (c) 2015年 Kotaro Suto. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+    @IBOutlet var intLabel:UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    @IBAction func one(){
+        
+    }
+    @IBAction func two(){
+        
+    }
+    @IBAction func three(){
+        
+    }
+}
